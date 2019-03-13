903bfe3f7811f1a181f2d36e847653fb8c020e9c
wuhuisheng <wuhuisheng@hlchang.cn>
2019-03-13 20:22:53 +0800

    修改参数类型

--- auth.go
+++ auth.go
@@ -16,7 +16,7 @@ type BaseResp struct {
 type AuthuserResp struct {
 	Openid    string   //用户的唯一标识
 	Nickname  string   //用户昵称
-	Sex       string   //用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+	Sex       int   //用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
 	Province  string   //用户个人资料填写的省份
 	City      string   //普通用户个人资料填写的城市
 	Country   string   //国家，如中国为CN
@@ -31,19 +31,19 @@ type UserResp struct {
 	Subscribe	string//用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
 	Openid	string//用户的标识，对当前公众号唯一
 	Nickname	string//用户的昵称
-	Sex	string//用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+	Sex	int//用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
 	City	string//用户所在城市
 	Country	string//用户所在国家
 	Province	string//用户所在省份
 	Language	string//用户的语言，简体中文为zh_CN
 	Headimgurl	string//用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
-	Subscribe_time	string//用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
+	Subscribe_time	int//用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
 	Unionid	string//只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
 	Remark	string//公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
-	Groupid	string//用户所在的分组ID（兼容旧的用户分组接口）
-	Tagid_list	string//用户被打上的标签ID列表
+	Groupid	int//用户所在的分组ID（兼容旧的用户分组接口）
+	Tagid_list	[]int//用户被打上的标签ID列表
 	Subscribe_scene	string//返回用户关注的渠道来源，ADD_SCENE_SEARCH 公众号搜索，ADD_SCENE_ACCOUNT_MIGRATION 公众号迁移，ADD_SCENE_PROFILE_CARD 名片分享，ADD_SCENE_QR_CODE 扫描二维码，ADD_SCENEPROFILE LINK 图文页内名称点击，ADD_SCENE_PROFILE_ITEM 图文页右上角菜单，ADD_SCENE_PAID 支付后关注，ADD_SCENE_OTHERS 其他
-	Qr_scene	string//二维码扫码场景（开发者自定义）
+	Qr_scene	int//二维码扫码场景（开发者自定义）
 	Qr_scene_str	string//二维码扫码场景描述（开发者自定义
 }
 
